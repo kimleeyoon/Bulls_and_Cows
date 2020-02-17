c63a528d02aa22328c65b6c574573c786a767af7
JongwonYoun <jeffyoun@naver.com>
2020-02-17 22:47:31 +0900

    error문 주석

--- app.js
+++ app.js
@@ -40,12 +40,12 @@ router.route("/WinGameAction2").post((req, res, next) => {
 
 app.use("/", router);
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
+// // catch 404 and forward to error handler
+// app.use((req, res, next) => {
+//   const err = new Error("Not Found");
+//   err.status = 404;
+//   next(err);
+// });
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running on ${SERVER_PORT} port`);
