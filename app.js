83ec0ec0c477918a604b3b6c025b51539a24af50
JongwonYoun <jeffyoun@naver.com>
2020-02-18 18:02:21 +0900

    express -> koa

--- app.js
+++ app.js
@@ -1,60 +1,60 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
+// const express = require("express");
+const Koa = require("koa");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const nugu = require("./nugu");
 
-const {
-  SERVER_PORT
-} = require("./config.js");
-const routes = require("./routes");
+const { SERVER_PORT } = require("./config.js");
+// const routes = require("./routes");
+const Router = require("koa-router");
 
-const app = express();
-let router = express.Router();
+// const app = express();
+const app = new Koa();
+// let router = express.Router();
+let router = new Router();
 app.use(bodyParser.json());
 app.use(morgan("common"));
 app.use((err, req, res, next) => next());
 
 let users = {};
 
-// app.use('/', routes);
-router.route("/GameStartAction").post((req, res, next) => {
-  console.log("GameStartAction");
-  nugu(req, res, next, users);
-});
-router.route("/ResultAction").post((req, res, next) => {
-  console.log("ResultAction");
-  nugu(req, res, next, users);
-});
-router.route("/WinGameAction").post((req, res, next) => {
-  console.log("WinGameAction");
-  nugu(req, res, next, users);
-});
-router.route("/ResultAction2").post((req, res, next) => {
-  console.log("ResultAction2");
-  nugu(req, res, next, users);
-});
-router.route("/addAction").post((req, res, next) => {
-  console.log("addAction");
-  nugu(req, res, next, users);
-});
-router.route("/CheckAction").post((req, res, next) => {
-  console.log("CheckAction");
-  nugu(req, res, next, users);
-});
-router.route("/WinGameAction2").post((req, res, next) => {
-  console.log("WinGameAction2");
-  nugu(req, res, next, users);
-});
-
-app.use("/", router);
-
-// // catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   const err = new Error("Not Found");
-//   err.status = 404;
-//   next(err);
+// router.route("/GameStartAction").post((req, res, next) => {
+//   console.log("GameStartAction");
+//   nugu(req, res, next, users);
 // });
+router.post("/GameStartAction", (ctx, next) => {
+  console.log("GameStartAction");
+  console.log(ctx.params);
+
+  nugu(ctx, users);
+});
+router.post("/ResultAction", (ctx, next) => {
+  console.log("ResultAction");
+  nugu(ctx, users);
+});
+router.post("/WinGameAction", (ctx, next) => {
+  console.log("WinGameAction");
+  nugu(ctx, users);
+});
+router.post("/ResultAction2", (ctx, next) => {
+  console.log("ResultAction2");
+  nugu(ctx, users);
+});
+router.post("/addAction", (ctx, next) => {
+  console.log("addAction");
+  nugu(ctx, users);
+});
+router.post("/CheckAction", (ctx, next) => {
+  console.log("CheckAction");
+  nugu(ctx, users);
+});
+router.post("/WinGameAction2", (ctx, next) => {
+  console.log("WinGameAction2");
+  nugu(ctx, users);
+});
+
+// app.use("/", router);
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(SERVER_PORT, () => {
   console.log(`Server is running on ${SERVER_PORT} port`);
